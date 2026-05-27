70efe7c1e234ea1e2b68136ba6f27ecb811a5d0f
Franco Heis <franco.heis2008@gmail.com>
2026-05-27 00:04:07 -0300

    Implement main function and configure I/O settings in Barrier.h

--- Barrier.h
+++ Barrier.h
@@ -1,2 +1,31 @@
-#include <xc.h>
+/* Define crystal frequency before including xc.h so delay macros work */
 #define _XTAL_FREQ 4000000
+#include <xc.h>
+#define __delay_ms(x) 
+
+void main(void){
+    CMCON = 7;
+    TRISA = 0b00111111;
+    TRISB = 0b00110111;
+    TRISA7 = 0;
+    TRISB3 = 0;
+    TRISB4 = 0;
+    TRISB5 = 0;
+    T2CON = 0b00000101;
+    PR2 = 100;
+    CCPR1L = 0;
+    CCP1CON = 0b00001100;
+    int inicio = 0;
+    RB4 = 0;
+    RB7 = 0;
+    RB5 = 0;
+    int sentido = 0;
+
+    while(1){
+        if (RB1==1){
+            inicio = !inicio;
+            __delay_ms(300);
+            sentido = 0;
+        }
+    }
+}
